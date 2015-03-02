b5821945bf0eed94020541a3bffeefcc93d0991b
PhilippeAssis <lippejc@gmail.com>
2015-03-02 09:53:40 -0300

    Add Update Objects

--- parallax-objects.js
+++ parallax-objects.js
@@ -1,7 +1,8 @@
 /**
-* Parallax Objects
-* */(function ($) {
-    $.fn.creatorObjects = function(objects,options){
+ * Parallax Objects
+ * */
+(function ($) {
+    $.fn.pxoCreatorObjects = function(objects,options){
         function isNotUndefined(obj){
             if(typeof obj == 'undefined')
                 return false;
@@ -60,6 +61,32 @@
         }
     };
 
+    $.fn.pxoUpdateObjects = function(object,options){
+        if(object === 0)
+            object = '0'
+
+        obj = $('.parallaxObjects-Object'+object, this);
+
+
+        if(options.speed)
+            obj.attr('pxoSpeed',options.speed)
+
+        if(options.limit)
+            obj.attr('pxoLimit',options.limit)
+
+        if(options.y)
+            obj.attr('pxoY',options.y)
+
+        if(options.x)
+            obj.attr('pxoX',options.x)
+
+        if(options.zoom)
+            obj.attr('pxoZ',options.zoom)
+
+        if(options.opacity)
+            obj.attr('pxoO',options.opacity)
+    };
+
     $.fn.parallaxObjects = function(data,custom){
 
         var target = $(this);
@@ -74,7 +101,7 @@
             speedRandom : false, // Velocidade de elementos aleatorias
             viewport : 200,// Determina quando a animação inicia
             viewportBottom : true, //Usa a base para determinar a distancia do viewport
-            noEnd : false, //Animação não tem fim, mesmo que o viewport ultrapasse ou seja limitado,
+            noEnd : false, //AnimaÃ§Ã£o nÃ£o tem fim, mesmo que o viewport ultrapasse ou seja limitado,
             limit : false, //limita o movimento do elemento
             mobile : false, //customiza os elementos em modo mobile
             mobileAnimate : true, //ativa ou desativa a animação em modo mobile
@@ -85,7 +112,7 @@
         if(typeof(custom) == 'object')
             options = $.extend(options, custom);
 
-        target.creatorObjects(data,options);
+        target.pxoCreatorObjects(data,options);
 
         var objects = $('.parallaxObjects-WrapRelative', target).find('.parallaxObjects');
 
@@ -117,8 +144,8 @@
             options.viewport = $(window).height() - options.viewport;
 
         /*
-        * Parallax
-        * */
+         * Parallax
+         * */
         var parallax = function(){
             var top = $(window).scrollTop();
 
@@ -216,8 +243,6 @@
             }
 
         };
-
-        parallax();
 
         $(window).on('touchmove scroll',function(){
             parallax();
